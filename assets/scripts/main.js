da10239bb73398c0acfa2138a76c5adc13630ac6
crisrocha025 <crisfabricio0104@gmail.com>
2021-01-05 18:11:56 -0200

    refazendo

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,14 +1,14 @@
-/* Menu Nav */
-const links = document.querySelectorAll('.menu ul li')
-
-links.forEach((link,ind,linkss) => {
-	link.addEventListener('click',() =>{
-		linkss[ind].classList.add('link_active')
-		for(let i=0;i<linkss.length;i++){
-			if(i != ind){
-				linkss[i].classList.remove('link_active')
-			}
-			
-		}
-	})
+/* Menu Dropdown */
+const dropdown = document.querySelector('.dropdown')
+const sub_menu = document.querySelector('.sub_menu')
+dropdown.addEventListener('click', () => {
+	sub_menu.classList.toggle('sub_menu_active')
+	dropdown.classList.toggle('dropdown_active')
+})
+/* Menu Toggle */
+const menu_toggle = document.querySelector('.menu_toggle')
+const header = document.querySelector('.header2')
+menu_toggle.addEventListener('click', () => {
+	header.classList.toggle('header_active')
+	menu_toggle.classList.toggle('menu_toggle_active')
 })
