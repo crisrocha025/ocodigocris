66d34b36d8acf4a99a0df6d2f2dab928633f8ac4
crisrocha025 <crisfabricio0104@gmail.com>
2021-01-06 17:54:15 -0200

    seção sobre

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -12,3 +12,41 @@ menu_toggle.addEventListener('click', () => {
 	header.classList.toggle('header_active')
 	menu_toggle.classList.toggle('menu_toggle_active')
 })
+
+
+//Anima ao scroll
+const debounce = function (func, wait, immediate) {
+	let timeout
+	return function (...args) {
+	  const context = this
+	  const later = function () {
+		timeout = null
+		if (!immediate) func.apply(context, args)
+	  }
+	  const callNow = immediate && !timeout
+	  clearTimeout(timeout)
+	  timeout = setTimeout(later, wait)
+	  if (callNow) func.apply(context, args)
+	}
+  }
+  
+  const target = document.querySelectorAll('[data-anime]')
+  
+  function animeScroll () {
+	const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4)
+	target.forEach(element => {
+	  if (windowTop > element.offsetTop) {
+		element.classList.add('animate')
+	  } else {
+		element.classList.remove('animate')
+	  }
+	})
+  }
+  
+  animeScroll()
+  
+  if (target.length) {
+	window.addEventListener('scroll', debounce(() => {
+	  animeScroll()
+	}, 200))
+  }
