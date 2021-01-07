a7a1a3c38a779c7336e3e376f77bc39b8043c7bd
crisrocha025 <crisfabricio0104@gmail.com>
2021-01-07 11:31:05 -0200

    seção templates terminado

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -13,40 +13,61 @@ menu_toggle.addEventListener('click', () => {
 	menu_toggle.classList.toggle('menu_toggle_active')
 })
 
+//Menu Templates
+const menu_toggle_templates = document.querySelector('.menu_toggle_templates')
+const menu_templates = document.querySelector('.menu_templates')
+menu_toggle_templates.addEventListener('click',() => {
+	menu_toggle_templates.classList.toggle('menu_toggle_templates_active')
+	menu_templates.classList.toggle('menu_templates_active')
+})
+ 
+let link_active = document.querySelectorAll('.menu_templates ul li')
+const templates_container = document.querySelector('.templates_content')
+const template_item = document.querySelectorAll('.templates_content #item')
 
-//Anima ao scroll
-const debounce = function (func, wait, immediate) {
-	let timeout
-	return function (...args) {
-	  const context = this
-	  const later = function () {
-		timeout = null
-		if (!immediate) func.apply(context, args)
-	  }
-	  const callNow = immediate && !timeout
-	  clearTimeout(timeout)
-	  timeout = setTimeout(later, wait)
-	  if (callNow) func.apply(context, args)
-	}
-  }
-  
-  const target = document.querySelectorAll('[data-anime]')
-  
-  function animeScroll () {
-	const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4)
-	target.forEach(element => {
-	  if (windowTop > element.offsetTop) {
-		element.classList.add('animate')
-	  } else {
-		element.classList.remove('animate')
-	  }
+template_item.forEach(el => {
+	el.classList.add('show_item')
+})
+
+link_active.forEach(item => {
+	item.addEventListener('click', () =>{
+		let activeClass = document.querySelector('.active')
+		activeClass.className = activeClass.className.replace("active","")
+		item.className += "active"
+
+		template_item.forEach(el => {
+			el.classList.remove('show_item')
+		})
+		
+			template_item.forEach(elem => {
+				if(item.innerText.toUpperCase() === "BLOGGER"){
+					if(elem.classList.value === "blogger"){
+						elem.classList.add('show_item')	
+					}else{
+						elem.classList.remove('show_item')
+					}
+				}else if(item.innerText.toUpperCase() === "RESPONSIVOS"){
+					if(elem.classList.value === "responsivos"){
+						elem.classList.add('show_item')	
+					}else{
+						elem.classList.remove('show_item')
+					}
+				}else if(item.innerText.toUpperCase() === "WORDPRESS"){
+					if(elem.classList.value === "wordpress"){
+						elem.classList.add('show_item')	
+					}else{
+						elem.classList.remove('show_item')
+					}
+				}else if(item.innerText.toUpperCase() === "PREMIUM"){
+					if(elem.classList.value === "premium"){
+						elem.classList.add('show_item')	
+					}else{
+						elem.classList.remove('show_item')
+					}
+				}else{
+					elem.classList.add('show_item')
+				}
+			})
+		
 	})
-  }
-  
-  animeScroll()
-  
-  if (target.length) {
-	window.addEventListener('scroll', debounce(() => {
-	  animeScroll()
-	}, 200))
-  }
+})
